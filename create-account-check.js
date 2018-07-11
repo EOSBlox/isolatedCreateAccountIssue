ed62b7c450c1aff8a99df73a17322429fad7c322
Will in China <email.will.in.china@gmail.com>
2018-07-11 22:32:02 +0800

    more logs

--- create-account-check.js
+++ create-account-check.js
@@ -60,29 +60,42 @@ class CreateAccountCheck extends PolymerElement {
 
   _start() {
     this._connect()
-    .then((eos) =>{
+    .then((eos) => {
       return this._makeAccount(eos)
     })
-    .then((result) =>{
+    .then((result) => {
+      console.log("-- Result")
       console.log(result)
     })
-    .catch((err) =>{
+    .catch((err) => {
+      console.log("-- Error")
       console.log(err)
     })
   }
 
 
   _connect(){
-    return new Promise((resolve, reject) => {
-
-      const keyProvider = this.shadowRoot.querySelector('#keyProvider').value;
+    return new Promise((resolve) => {
+      
+      const keyProviderVal = this.shadowRoot.querySelector('#keyProvider').value;
+      const keyProvider = [keyProviderVal];
       const httpEndpoint = "https://api.eosnewyork.io";
       const broadcast = true;
       const sign = true;
       const chainId = "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906";
       const expireInSeconds = 30;
+      const verbose = true;
 
-      const eos = Eos({keyProvider, httpEndpoint, broadcast, sign, chainId, expireInSeconds})
+      console.log("-- CONNECTING")
+      console.log('keyProvider: ' + keyProvider)
+      console.log('httpEndpoint: ' + httpEndpoint)
+      console.log('broadcast: ' + broadcast)
+      console.log('sign: ' + sign)
+      console.log('chainId: ' + chainId)
+      console.log('expireInSeconds: ' + expireInSeconds)
+      console.log('verbose: ' + verbose)
+
+      const eos = Eos({keyProvider, httpEndpoint, broadcast, sign, chainId, expireInSeconds, verbose})
       resolve(eos);
     })
   }
@@ -99,15 +112,30 @@ class CreateAccountCheck extends PolymerElement {
       const receiver = this.shadowRoot.querySelector('#newAccountName').value;
       const owner = this.shadowRoot.querySelector('#ownerPublicKey').value;
       const active = this.shadowRoot.querySelector('#activePublicKey').value;
-      const bytes = 8000;
-      const stake_net_quantity = '0.0200 EOS';
-      const stake_cpu_quantity = '0.0200 EOS';
+      const chainId = "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906";
+      const bytes = 7900;
+      const stake_net_quantity = '0.1000 EOS';
+      const stake_cpu_quantity = '0.1000 EOS';
       const transfer = 0;
 
+      console.log("-- MAKE ACCOUNT")
+      console.log('creator: ' + creator)
+      console.log('payer: ' + payer)
+      console.log('from: ' + from)
+      console.log('name: ' + name)
+      console.log('receiver: ' + receiver)
+      console.log('owner: ' + owner)
+      console.log('active: ' + active)
+      console.log('chainId: ' + chainId)
+      console.log('bytes: ' + bytes)
+      console.log('stake_net_quantity: ' + stake_net_quantity)
+      console.log('stake_cpu_quantity: ' + stake_cpu_quantity)
+      console.log('transfer: ' + transfer)
+
       eos.transaction(tr => {
-        tr.newaccount({creator, name, owner, active})
-        tr.buyrambytes({payer, receiver, bytes})
-        tr.delegatebw({from, receiver, stake_net_quantity, stake_cpu_quantity, transfer})
+        tr.newaccount({creator, name, owner, active, chainId})
+        tr.buyrambytes({payer, receiver, bytes, chainId})
+        tr.delegatebw({from, receiver, stake_net_quantity, stake_cpu_quantity, transfer, chainId})
       })
       .then((response) => {
         resolve(response)
